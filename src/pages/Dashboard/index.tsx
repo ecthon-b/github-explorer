10cad6afdd91756651c62a9dfb398c16195c5d89
Ecthon Borhis <ecthon@gmail.com>
2021-05-04 15:56:28 -0400

    🎨 Applying component style in Dashboarnd

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Title } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return <Title>Explorando repositórios no Github</Title>
 }
 export default Dashboard;
