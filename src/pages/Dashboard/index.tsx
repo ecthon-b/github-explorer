c620d759ea3b4d8abc81cd0a2d1cf492f5b4a8ff
Ecthon Borhis <ecthon@gmail.com>
2021-05-04 19:33:33 -0400

    🎨 Applying style on the Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explorando repositórios no Github</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+          <div>
+            <strong>ecthon/github-explorer</strong>
+            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+        <a href="teste">
+          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+          <div>
+            <strong>ecthon/github-explorer</strong>
+            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+        <a href="teste">
+          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+          <div>
+            <strong>ecthon/github-explorer</strong>
+            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+      </Repositories>
+    </>
+  );
 }
 export default Dashboard;
