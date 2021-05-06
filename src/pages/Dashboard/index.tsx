0420c3f4fbd1f5e9a061a50cdd0d61b20f97fbc1
Ecthon Borhis <ecthon@gmail.com>
2021-05-06 16:26:22 -0400

    ✏️ Adding static data

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -23,18 +23,26 @@ const Dashboard: React.FC = () => {
           <FiChevronRight size={20}/>
         </a>
         <a href="teste">
-          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+          <img src="https://github.com/Jozimarjo.png" alt="Ecthon"/>
           <div>
-            <strong>ecthon/github-explorer</strong>
-            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+            <strong>Jozimarjo/boom-world</strong>
+            <p>Script pra dominar o mundo</p>
           </div>
           <FiChevronRight size={20}/>
         </a>
         <a href="teste">
-          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+          <img src="https://github.com/programador66.png" alt="Ecthon"/>
           <div>
-            <strong>ecthon/github-explorer</strong>
-            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+            <strong>programador66/franguim-de-boa</strong>
+            <p>Script para ensinar a fazer frango assado de panela</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+        <a href="teste">
+          <img src="https://github.com/rayque.png" alt="Ecthon"/>
+          <div>
+            <strong>rayque/rack-for-tetoes</strong>
+            <p>Script para tetões infinitos no Age of Empires Doix</p>
           </div>
           <FiChevronRight size={20}/>
         </a>
