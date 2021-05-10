aca2eb9a075b4eef4c7daa48d413eb08b9770228
Ecthon Borhis <ecthon@gmail.com>
2021-05-10 13:54:18 -0400

    🔗 Connecting to the Github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,51 +1,57 @@
-import React from 'react';
+import React, {useState, FormEvent} from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+    // Adição de um novo repositório
+    // Consumir API do Github
+    // Salva novo repositório no estado
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer"/>
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório"/>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={ (e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"/>
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://github.com/ecthon.png" alt="Ecthon"/>
+        {repositories.map(repository =>(
+          <a key={repository.full_name} href="teste">
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
           <div>
-            <strong>ecthon/github-explorer</strong>
-            <p>Projeto feito durante a bootcamp GoStack da Rocketseat</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20}/>
         </a>
-        <a href="teste">
-          <img src="https://github.com/Jozimarjo.png" alt="Ecthon"/>
-          <div>
-            <strong>Jozimarjo/boom-world</strong>
-            <p>Script pra dominar o mundo</p>
-          </div>
-          <FiChevronRight size={20}/>
-        </a>
-        <a href="teste">
-          <img src="https://github.com/programador66.png" alt="Ecthon"/>
-          <div>
-            <strong>programador66/franguim-de-boa</strong>
-            <p>Script para ensinar a fazer frango assado de panela</p>
-          </div>
-          <FiChevronRight size={20}/>
-        </a>
-        <a href="teste">
-          <img src="https://github.com/rayque.png" alt="Ecthon"/>
-          <div>
-            <strong>rayque/rack-for-tetoes</strong>
-            <p>Script para tetões infinitos no Age of Empires Doix</p>
-          </div>
-          <FiChevronRight size={20}/>
-        </a>
+        ))}
       </Repositories>
     </>
   );
