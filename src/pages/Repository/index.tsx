ba3ab4de4051b5030d51fb7c416068d1ad6a5868
Ecthon Borhis <ecthon@gmail.com>
2021-05-13 15:10:36 -0400

    🧾 Listing API issues

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
 
@@ -8,8 +9,50 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string,
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+    // }
+    // loadData();
+  }, [params.repository]);
 
   return (
     <>
@@ -20,38 +63,43 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
-      <RepositoryInfo>
+
+      { repository && (
+        <RepositoryInfo>
         <header>
-          <img src="https://github.com/ecthon.png" alt="Ecthon" />
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <div>
-            <strong>ecthon/nlw-ecoleta</strong>
-            <p>descrição do repositório</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
         </header>
         <ul>
           <li>
-            <strong>1809</strong>
+            <strong>{repository.stargazers_count}</strong>
             <span>Stars</span>
           </li>
           <li>
-            <strong>67</strong>
+            <strong>{repository.forks_count}</strong>
             <span>Forks</span>
           </li>
           <li>
-            <strong>67</strong>
+            <strong>{repository.open_issues_count}</strong>
             <span>Issues abertas</span>
           </li>
         </ul>
       </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link to="textejasbd">
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
           <div>
-            <strong>asdasd</strong>
-            <p>asasasa</p>
+            <strong>{issue.title}</strong>
+            <p>{issue.user.login}</p>
           </div>
           <FiChevronRight size={20}/>
-        </Link>
+        </a>
+        ))}
       </Issues>
     </>
   );
