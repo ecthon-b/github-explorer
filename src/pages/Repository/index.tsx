9d37a31583bef5fb939d42bd28a5bbd67cd054a1
Ecthon Borhis <ecthon@gmail.com>
2021-05-13 11:56:41 -0400

    🎨 Applying style on the Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,6 +10,50 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16}/>
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://github.com/ecthon.png" alt="Ecthon" />
+          <div>
+            <strong>ecthon/nlw-ecoleta</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1809</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="textejasbd">
+          <div>
+            <strong>asdasd</strong>
+            <p>asasasa</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </Link>
+      </Issues>
+    </>
+  );
 }
 export default Repository;
